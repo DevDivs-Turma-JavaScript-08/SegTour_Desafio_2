b84008db0d0c3f045290f9fbe1c4e97cc2f5a916
Agataandrade <agatasouandrade@gmail.com>
2025-08-01 12:52:52 -0300

    feat:adiciona tabelas

--- src/clientes/entities/clientes.entities.ts
+++ src/clientes/entities/clientes.entities.ts
@@ -1,1 +1,46 @@
-// Client entities
+import { IsNotEmpty } from "class-validator";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Seguros } from "../../seguros/entities/seguros.entities";
+
+
+@Entity({name: "tb_clientes"})
+export class Clientes{
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @IsNotEmpty()
+    @Column({length: 100, nullable: false})
+    nome: string;
+
+    
+    @IsNotEmpty()
+    @Column({length: 100, nullable: false})
+    email: string;
+
+    
+    @IsNotEmpty()
+    @Column({length: 20, nullable: false})
+    telefone: string;
+
+    
+    @IsNotEmpty()
+    @Column({length: 10, nullable: false})
+    dataNascimento: string;
+
+
+    @IsNotEmpty()
+    @Column({length: 20, nullable: false})
+    cpf: string;
+
+
+    @IsNotEmpty()
+    @Column({length: 100, nullable: false})
+    endereço: string;
+
+    @ManyToOne(()=> Seguros, (seguros) => seguros.clientes,{
+        onDelete: 'CASCADE'
+
+ })
+ seguros: Seguros;
+}
--- src/seguros/entities/seguros.entities.ts
+++ src/seguros/entities/seguros.entities.ts
@@ -1,1 +1,40 @@
-// CRIAR ENTITIES
+import { IsNotEmpty } from "class-validator";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Clientes } from "../../clientes/entities/clientes.entities";
+
+
+@Entity({name: "tb_seguros"})
+export class Seguros {
+
+   @PrimaryGeneratedColumn()
+   id: number
+
+   @IsNotEmpty()
+   @Column({length:100, nullable: false})
+   numeroApolice: string;
+
+   @IsNotEmpty()
+   @Column({length:100, nullable: false})
+   destino: string;
+
+   @IsNotEmpty()
+   @Column({length:100, nullable: false})
+   tipoCobertura: string;
+
+   @IsNotEmpty()
+   @Column({nullable: false})
+   valor: number;
+
+   @IsNotEmpty()
+   @Column({length:100, nullable: false})
+   moeda: string;
+
+  
+   @IsNotEmpty()
+   @Column({nullable: false})
+   tempoViagem: number;
+
+@OneToMany(() => Clientes, (clientes) => Clientes.seguros)
+    clientes: Clientes[];
+
+}
