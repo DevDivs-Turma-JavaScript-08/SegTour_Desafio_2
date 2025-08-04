c0a0f042622cfba546da40d1f73447a3e281b1bb
Agataandrade <agatasouandrade@gmail.com>
2025-08-04 08:52:26 -0300

    fix:ajusta entities

--- src/clientes/entities/clientes.entities.ts
+++ src/clientes/entities/clientes.entities.ts
@@ -1,41 +1,65 @@
-import { IsNotEmpty } from 'class-validator';
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
-import { Seguros } from '../../seguros/entities/seguros.entities';
+import { IsNotEmpty } from "class-validator";
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Seguros } from "../../seguros/entities/seguros.entities";
 
-@Entity({ name: 'tb_clientes' })
-export class Clientes {
-  @PrimaryGeneratedColumn()
-  id: number;
 
-  @IsNotEmpty()
-  @Column({ length: 100, nullable: false })
-  nome: string;
+@Entity({name: "tb_clientes"})
+export class Clientes{
 
-  @IsNotEmpty()
-  @Column({ length: 100, nullable: false })
-  email: string;
+    @PrimaryGeneratedColumn()
+    id: number;
 
-  @IsNotEmpty()
-  @Column({ length: 20, nullable: false })
-  telefone: string;
+    @IsNotEmpty()
+    @Column({length: 100, nullable: false})
+    nome: string;
 
-  @IsNotEmpty()
-  @Column({ length: 10, nullable: false })
-  dataNascimento: string;
+    
+    @IsNotEmpty()
+    @Column({length: 100, nullable: false})
+    email: string;
 
-  @IsNotEmpty()
-  @Column({ length: 20, nullable: false })
-  cpf: string;
+    
+    @IsNotEmpty()
+    @Column({type:"bigint", nullable: false})
+    telefone: number;
 
-  @IsNotEmpty()
-  @Column({ length: 100, nullable: false })
-  endereço: string;
+    
+    @IsNotEmpty()
+    @Column({type: "date", nullable: false})
+    dataNascimento: Date;
 
-  @ManyToOne(() => Seguros, (seguros) => seguros.clientes, {
-    onDelete: 'CASCADE',
-  })
-  seguros: Seguros;
-  static seguros: any;
+
+    @IsNotEmpty()
+    @Column({type: "bigint", nullable: false})
+    cpf: number;
+
+
+    @IsNotEmpty()
+    @Column({length: 100, nullable: false})
+    endereço: string;
+
+   @IsNotEmpty()
+   @Column({length:100, nullable: false})
+   numeroApolice: string;
+
+   @IsNotEmpty()
+   @Column({nullable: false})
+   tempoViagem: number;
+
+   @IsNotEmpty()
+   @Column({type:"decimal", precision:10,scale:2, nullable: false})
+   valor: number;
+
+
+   @Column({type:"decimal", precision:10,scale:2, default:0})
+   valorFinal: number;
+
+
+    @ManyToOne(()=> Seguros, (seguros) => seguros.clientes,{
+        onDelete: 'CASCADE'
+
+ })
+ @JoinColumn({name: "seguros_id"})
+ seguros: Seguros;
+    
 }
-
-export { Seguros };
